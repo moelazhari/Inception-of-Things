b322787ed9ef6369228f43b6909d5799cac1382a
Mohamed elazhari <mazhari@student.1337.ma>
2025-05-03 17:18:01 +0100

    getting k3s token using ssh instead of shred folder

--- p1/tools/setupS.sh
+++ p1/tools/setupS.sh
@@ -1,8 +1,5 @@
-
-
 mkdir -p /home/vagrant/.ssh
 echo "$1" >> /home/vagrant/.ssh/authorized_keys
 
 # install k3s
-curl -sfL https://get.k3s.io | sh -
-sudo cp /var/lib/rancher/k3s/server/token /node-token/
+curl -sfL https://get.k3s.io |  INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644" sh -
--- p1/tools/setupSW.sh
+++ p1/tools/setupSW.sh
@@ -1,7 +1,13 @@
-
-
 mkdir -p /home/vagrant/.ssh
 echo "$1" >> /home/vagrant/.ssh/authorized_keys
 
+SERVER_IP="192.168.56.110"
+
+KEY_PATH="/vagrant/.vagrant/machines/ael-korcS/virtualbox/private_key"
+
+SSH_OPTS="-o StrictHostKeyChecking=no"
+
+TOKEN=$(ssh $SSH_OPTS -i "$KEY_PATH" vagrant@$SERVER_IP "sudo cat /var/lib/rancher/k3s/server/node-token")
+
 # install k3s
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=$(cat /node-token/token) sh -
+curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$TOKEN sh -
